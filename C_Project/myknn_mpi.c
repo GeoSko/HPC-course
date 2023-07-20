64a5f3908dd62668ba74d8341634e31a00bd4f68
Georgios Skondras <gskondras@upnet.gr>
2023-07-20 23:19:48 +0300

    mpi done

--- C_Project/myknn_mpi.c
+++ C_Project/myknn_mpi.c
@@ -51,6 +51,11 @@ double find_knn_value(double *p, int n, int knn)
 
 int main(int argc, char *argv[])
 {
+	MPI_Init(&argc, &argv);
+	int rank;
+	int size;
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
 	
 
 	if (argc != 3)
@@ -86,14 +91,16 @@ int main(int argc, char *argv[])
 
 	double *y = malloc(QUERYELEMS*sizeof(double));
 
-	double t0, t1, t_first = 0.0, t_sum = 0.0;
-	double sse = 0.0;
-	double err, err_sum = 0.0;
+	// double t0, t1;
+	double t_first = 0.0, t_sum_local = 0.0, t_sum_global = 0.0;
+	double local_sse = 0.0;
+	double global_sse = 0.0;
+	double err, global_err_sum = 0.0, local_err_sum = 0.0;
 	double x[PROBDIM];
 
 	double *xmem_test = (double *)malloc(QUERYELEMS*PROBDIM*sizeof(double));
 	xtest = (double **)malloc(QUERYELEMS*sizeof(double *));
-	#pragma omp parallel for
+
 	for (int i = 0; i < QUERYELEMS; i++) xtest[i] = xmem_test + i*PROBDIM; //&mem[i*PROBDIM];
 
 	for (int i=0;i<QUERYELEMS;i++) {
@@ -117,45 +124,66 @@ int main(int argc, char *argv[])
 // 		y[i] = 0.0;
 // #endif
 
-	t0 = omp_get_wtime();
-	#pragma omp parallel for private(t0,t1,err) reduction(+:t_sum,sse,err_sum)
-	for (int i=0;i<QUERYELEMS;i++) {
+	int batch = QUERYELEMS/size;
+	int start = rank*batch;
+	int end = start+batch;
+
+	// printf("rank: %d\nqueryelems: %d\nbatch: %d\nstart: %d\nend:%d\n\n",rank, QUERYELEMS, batch, start, end);
+
+
+
+	// t0 = omp_get_wtime();
+	double t1 = MPI_Wtime();
+	for (int i=start;i<end;i++) {
 		// t0 = gettime();
 		
 		double yp = find_knn_value(xtest[i], PROBDIM, NNBS);
 		// t1 = gettime();
 
-		sse += (ytest[i]-yp)*(ytest[i]-yp);
+		local_sse += (ytest[i]-yp)*(ytest[i]-yp);
 
 		//for (k = 0; k < PROBDIM; k++)
 		//	fprintf(fpout,"%.5f ", x[k]);
 
 		err = 100.0*fabs((yp-ytest[i])/ytest[i]);
 		//fprintf(fpout,"%.5f %.5f %.2f\n", y[i], yp, err);
-		err_sum += err;
+		local_err_sum += err;
 
 	}
-	t1 = omp_get_wtime();
-	t_sum = (t1-t0);
+	// t1 = omp_get_wtime();
+	double t2 = MPI_Wtime();
+	double t_local = t2-t1;
+	// t_sum_local = (t1-t0);
 	// fclose(fpin);
 	//fclose(fpout);
 
-	double mse = sse/QUERYELEMS;
-	double ymean = compute_mean(ytest, QUERYELEMS);
-	double var = compute_var(ytest, QUERYELEMS, ymean);
-	double r2 = 1-(mse/var);
+	MPI_Reduce(&local_sse, &global_sse, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(&local_err_sum, &global_err_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	// MPI_Reduce(&t_sum_local, &t_sum_local, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-	printf("Results for %d query points\n", QUERYELEMS);
-	printf("APE = %.2f %%\n", err_sum/QUERYELEMS);
-	printf("MSE = %.6f\n", mse);
-	printf("R2 = 1 - (MSE/Var) = %.6lf\n", r2);
 
-	t_sum = t_sum*1000.0;			// convert to ms
-	t_first = t_first*1000.0;	// convert to ms
-	printf("Total time = %lf ms\n", t_sum);
-	// printf("Time for 1st query = %lf ms\n", t_first);
-	// printf("Time for 2..N queries = %lf ms\n", t_sum-t_first);
-	printf("Average time/query = %lf ms\n", (t_sum)/(QUERYELEMS));
+	if (rank==0){ // only one prints
+		printf("rank %d:\n", rank);
+		double mse = global_sse/QUERYELEMS;
+		
+		double ymean = compute_mean(ytest, QUERYELEMS);
+		double var = compute_var(ytest, QUERYELEMS, ymean);
+		double r2 = 1-(mse/var);
 
+		printf("Results for %d query points\n", QUERYELEMS);
+		printf("APE = %.2f %%\n", global_err_sum/QUERYELEMS);
+		printf("MSE = %.6f\n", mse);
+		printf("R2 = 1 - (MSE/Var) = %.6lf\n", r2);
+
+		t_local = t_local*1000.0;			// convert to ms
+		// t_first = t_first*1000.0;	// convert to ms
+		printf("Total time = %lf ms\n", t_local);
+		// printf("Time for 1st query = %lf ms\n", t_first);
+		// printf("Time for 2..N queries = %lf ms\n", t_sum-t_first);
+		printf("Average time/query = %lf ms\n", (t_local)/(QUERYELEMS));
+	} 
+
+	
+	MPI_Finalize();
 	return 0;
 }
